0a8f699d4386f1caff4cee77801e9a0b44bb1b8f
Mikko Piaggio <mikkopk48@gmail.com>
2025-05-27 11:18:06 -0300

    Add initial HTML structure for ToDo List application

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aplicación ToDo List Laravel</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="task" id="task">
+        <input type="button" name="Enviar">
+    </form>
+
+</body>
+
+</html>
